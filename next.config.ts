58a5260118da4efb69bf691663189530f359d7f7
Jari Peltola <jena9988@gmail.com>
2025-12-15 17:11:03 +0200

    Picture resizing

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
   },
   images: {
     qualities: [70, 75, 85, 90],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
+    formats: ['image/webp'],
   },
 };
 
